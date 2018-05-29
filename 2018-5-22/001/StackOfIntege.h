aaec22145d7734b04bf3bf7bb7c6f6063235b47e
4060E027 <37572140+4060E027@users.noreply.github.com>
2018-05-29 14:13:00 +0800

    Update StackOfIntege.h

--- 2018-5-22/001/StackOfIntege.h
+++ 2018-5-22/001/StackOfIntege.h
@@ -3,17 +3,17 @@
 
 class StackOfIntegers
 {
-public:
-  StackOfIntegers();
-  bool isEmpty() const;
-  int peek() const;
-  void push(int value);
-  int pop();
-  int getSize() const;
+public:                        //公開的方法：可以被純取
+  StackOfIntegers();           //建構子
+  bool isEmpty() const;        //判斷是否為空的函數
+  int peek() const;            //取出最上面元數的函數
+  void push(int value);        //把資料放入的函數
+  int pop();                   //取出最上面資料的函數
+  int getSize() const;         //取得堆疊大小的函數
 
-private:
-  int elements[100];
-  int size;
+private:                       //將資料庫影藏
+  int elements[100];           //大小為100
+  int size;                    //設一個值
 };
 
 #endif
