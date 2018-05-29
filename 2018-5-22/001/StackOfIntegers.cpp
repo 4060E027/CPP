55d12bf0cc7101e25e08cdad698579c8756bfcdb
4060E027 <37572140+4060E027@users.noreply.github.com>
2018-05-29 14:02:10 +0800

    Update StackOfIntegers.cpp

--- 2018-5-22/001/StackOfIntegers.cpp
+++ 2018-5-22/001/StackOfIntegers.cpp
@@ -1,31 +1,31 @@
 #include "StackOfIntege.h"
 
-StackOfIntegers::StackOfIntegers()
+StackOfIntegers::StackOfIntegers()        //建構值
 {
-  size = 0;
+  size = 0;                               //大小等於零
 }
 
-bool StackOfIntegers::isEmpty() const
+bool StackOfIntegers::isEmpty() const     //判斷isEmpty函數
 {
-  return (size == 0);
+  return (size == 0);                     //大小等於零回傳true否則回傳false
 }
 
-int StackOfIntegers::peek() const
+int StackOfIntegers::peek() const         //peek函數
 {
-  return elements[size - 1];
+  return elements[size - 1];              //回傳最上面一位的值
 }
 
-void StackOfIntegers::push(int value)
+void StackOfIntegers::push(int value)     //push函數
 {
-  elements[size++] = value;
+  elements[size++] = value;               //將一個值放進資料庫
 }
 
-int StackOfIntegers::pop()
+int StackOfIntegers::pop()                //pop函數
 {
-  return elements[--size];
+  return elements[--size];                //將最上面的值取出
 }
 
-int StackOfIntegers::getSize() const
+int StackOfIntegers::getSize() const      //getSize函數
 {
-  return size;
+  return size;                            //會傳目前的大小值
 }
